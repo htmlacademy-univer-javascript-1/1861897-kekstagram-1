5ac5fc195fa07a9f64e0eb8b58fa782bf4faab05
Polina <polina-postylyakova@yandex.ru>
2022-12-11 18:29:45 +0300

    task 12_1 is ready and corrections for 11th task are made

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-import {renderPictures} from './pictures.js';
+export { faker } from 'https://cdn.skypack.dev/@faker-js/faker';
+import {initialRenderPictures} from './pictures.js';
 import './image-upload.js';
 import {getData} from './api.js';
 import {showAlert} from './utils.js';
 
 getData((pictures) => {
-  renderPictures(pictures);
+  initialRenderPictures(pictures);
 },
 () => {
   showAlert('Не удалось загрузить данные. Проверьте подключение к Интернету и попробуйте перезагрузить страницу.');
